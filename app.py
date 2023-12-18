2185426447dbfb64bd25301ad5d826d060f671de
metcalfc <metcalfc@gmail.com>
2023-12-18 23:40:43 +0000

    Making this more jetbrains friendly.

--- app.py
+++ app.py
@@ -1,34 +1,21 @@
-#-----------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------
 from os import environ as env
-import argparse
 from flask import Flask
 
 app = Flask(__name__)
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-w", "--workspace",
-                    default=env.get('DAYTONA_WS_ID', 'user-repo-abcd1234'))
-parser.add_argument("-d", "--domain",
-                    default=env.get('DAYTONA_WS_DOMAIN', 'daytona.io'))
-parser.add_argument("-p", "--port", type=int,
-                    default=5000,
-                    metavar="[1024-65534]",
-                    choices=range(1024,65534))
+workspace = env.get('DAYTONA_WS_ID', 'user-repo-abcd1234')
+domain = env.get('DAYTONA_WS_DOMAIN', 'daytona.io')
+port = 5000
 
-args = parser.parse_args()
-
-workspace = args.workspace
-domain = args.domain
-port = args.port
 
 @app.route("/")
 def hello():
     return "Hello World! from https://" + str(port) + "-" + workspace + "." + domain
 
+
 if __name__ == "__main__":
-   app.run(host="localhost", port=port, debug=True)
-
-
+    app.run(host="localhost", port=port, debug=True)
